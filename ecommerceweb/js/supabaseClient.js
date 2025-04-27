d3e0d3d692b1029968e64e8967bbd89ae03f721f
stream <yanamarco017@gmail.com>
2025-04-27 18:45:16 -0500

    Update supabaseClient.js

--- ecommerceweb/js/supabaseClient.js
+++ ecommerceweb/js/supabaseClient.js
@@ -1,8 +1,6 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+// js/supabaseClient.js
 
-// Primero creamos el cliente de Supabase
-export const supabase = createClient(
-  'https://brzsayjqohyhpssfgqct.supabase.co', 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyenNheWpxb2h5aHBzc2ZncWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMTU5MzYsImV4cCI6MjA2MDU5MTkzNn0.4JNO1yeUcuSnJXOMN_bZRlugDQZFbkyqYgWyQYkUFF8'
-);
+const supabaseUrl = 'https://brzsayjqohyhpssfgqct.supabase.co'; // 👈 Reemplaza con tu URL de Supabase
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyenNheWpxb2h5aHBzc2ZncWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMTU5MzYsImV4cCI6MjA2MDU5MTkzNn0.4JNO1yeUcuSnJXOMN_bZRlugDQZFbkyqYgWyQYkUFF8'; // 👈 Reemplaza con tu anon key correcta
+
+const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
