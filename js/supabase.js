1dfb6d4901cc480999139ff2da03d31431c56edf
stream <yanamarco017@gmail.com>
2025-04-29 18:24:47 -0500

    Delete js directory

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-// Configuración de Supabase
-const supabaseUrl = 'https://brzsayjqohyhpssfgqct.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyenNheWpxb2h5aHBzc2ZncWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMTU5MzYsImV4cCI6MjA2MDU5MTkzNn0.4JNO1yeUcuSnJXOMN_bZRlugDQZFbkyqYgWyQYkUFF8';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
