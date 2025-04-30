eed83978fb01c29f6468dd79eb492811c728151f
stream <yanamarco017@gmail.com>
2025-04-30 16:15:16 -0500

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 // js/supabaseClient.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const SUPABASE_URL = 'https://brzsayjqohyhpssfgqct.supabase.co'  // Reemplaza si usas otro proyecto
-const SUPABASE_KEY = 'public-anon-key-aquí' // ← Usa tu clave pública de Supabase (anon/public key)
+const SUPABASE_URL = 'https://nvpmosrzmegcjoackltw.supabase.co'  // Reemplaza si usas otro proyecto
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cG1vc3J6bWVnY2pvYWNrbHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjU0NTcsImV4cCI6MjA2MTU0MTQ1N30.SUtjtmJ96r6zQm-GAJHNy6jj5lbu56DiPvVfZk9ffbI' // ← Usa tu clave pública de Supabase (anon/public key)
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
