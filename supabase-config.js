14932162288a0f24f9da6f181b6dae85dffefc63
stream <yanamarco017@gmail.com>
2025-04-29 22:27:15 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Configuración de Supabase
-const supabaseUrl = 'TU_URL_SUPABASE';
-const supabaseKey = 'TU_KEY_SUPABASE';
+const supabaseUrl = 'https://nvpmosrzmegcjoackltw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cG1vc3J6bWVnY2pvYWNrbHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjU0NTcsImV4cCI6MjA2MTU0MTQ1N30.SUtjtmJ96r6zQm-GAJHNy6jj5lbu56DiPvVfZk9ffbI';
 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
