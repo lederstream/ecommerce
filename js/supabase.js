43d5a2a5481849e08e7cf440301fcfe7a22feb99
stream <yanamarco017@gmail.com>
2025-04-29 22:25:40 -0500

    Delete js directory

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-// Primero cargar la biblioteca de Supabase
-const supabaseUrl = 'https://brzsayjqohyhpssfgqct.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyenNheWpxb2h5aHBzc2ZncWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMTU5MzYsImV4cCI6MjA2MDU5MTkzNn0.4JNO1yeUcuSnJXOMN_bZRlugDQZFbkyqYgWyQYkUFF8';
-
-// Crear instancia global después de asegurar que la biblioteca está cargada
-document.addEventListener('DOMContentLoaded', () => {
-    window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
-    console.log('Supabase inicializado correctamente');
-});
